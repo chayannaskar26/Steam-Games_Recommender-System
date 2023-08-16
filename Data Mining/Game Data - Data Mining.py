7d07481ad1f65f376e65d8506709314f7625bb53
chayannaskar26 <chayannaskar@live.com>
2023-08-16 18:13:21 +0530

    Added Files

---
+++ Data Mining/Game Data - Data Mining.py
@@ -0,0 +1,123 @@
+# Importing required libraries
+import pandas as pd
+import json
+from urllib.request import urlopen
+
+# Fetch Game Data Methods
+def getGameData(appid):
+    res = urlopen(f'https://store.steampowered.com/api/appdetails?appids={appid}&l=english')
+    res_json = json.load(res)
+    json_data = res_json[f'{appid}']['data']
+    return [
+        appid,
+        json_data['name'],
+        json_data['short_description'],
+        json_data['is_free'],
+        json_data['supported_languages'],
+        json_data['developers'],
+        json_data['publishers'],
+        json_data['platforms'],
+        json_data['categories'],
+        json_data['genres'],
+        json_data['release_date']['date'],
+    ]
+    
+# Declaring Coloumn Names
+column_names = ['appid', 'title', 'short_description', 'is_free', 'supported_languages', 'developers', 'publishers' ,'platforms', 'categories', 'genres', 'release_date']
+
+
+# Valve Dataset Game Data
+def getValveDetailsData():
+    valve_df = pd.read_csv('Steam Valve Games.csv')
+
+    req_json_list = []
+
+    for appid in valve_df['appid']:
+        req_json_list.append(getGameData(appid))
+        
+    valve_details_df = pd.DataFrame(req_json_list, columns=column_names)
+
+    return valve_details_df 
+
+
+# Activision Dataset Game Data
+def getActivisionDetailsData():
+    activision_df = pd.read_csv('Steam Activision Games.csv')
+
+    req_json_list = []
+
+    for appid in activision_df['appid']:
+        req_json_list.append(getGameData(appid))
+        
+    activision_details_df = pd.DataFrame(req_json_list, columns=column_names)
+
+    return activision_details_df 
+
+
+# Ubisoft Dataset Game Data 
+def getUbisoftDetailsData():
+    ubisoft_df = pd.read_csv('Steam Ubisoft Games.csv')
+
+    req_json_list = []
+
+    for appid in ubisoft_df['appid']:
+        req_json_list.append(getGameData(appid))
+        
+    ubisoft_details_df = pd.DataFrame(req_json_list, columns=column_names)
+
+    return ubisoft_details_df 
+
+# Bethesda Dataset Game Data
+def getBethesdaDetailsData():
+    bethesda_df = pd.read_csv('Steam Bethesda Games.csv')
+
+    req_json_list = []
+
+    for appid in bethesda_df['appid']:
+        req_json_list.append(getGameData(appid))
+    bethesda_details_df = pd.DataFrame(req_json_list, columns=column_names)
+
+    return bethesda_details_df 
+
+
+# EA Dataset Game Data 
+def getEADetailsData():
+    ea_df = pd.read_csv('Steam EA Games.csv')
+
+    req_json_list = []
+
+    for appid in ea_df['appid']:
+        req_json_list.append(getGameData(appid))
+    ea_details_df = pd.DataFrame(req_json_list, columns=column_names)
+
+    return ea_details_df 
+
+
+# Other Dataset Game Data 
+def getOtherDetailsData():
+    other_df = pd.read_csv('Steam Other Games.csv')
+    req_json_list = []
+
+    for appid in other_df['appid']:
+        req_json_list.append(getGameData(appid))
+    other_details_df = pd.DataFrame(req_json_list, columns=column_names)
+
+    return other_details_df 
+
+
+def main():
+    valve_details_df = getValveDetailsData()
+    activision_details_df = getActivisionDetailsData()
+    ubisoft_details_df = getUbisoftDetailsData()
+    bethesda_details_df = getBethesdaDetailsData()
+    ea_details_df = getEADetailsData()
+    other_details_df = getOtherDetailsData()
+    
+    # Concating to final dataframe
+    final_df = pd.concat([valve_details_df, activision_details_df, ubisoft_details_df, bethesda_details_df, ea_details_df, other_details_df])
+    final_df.reset_index(inplace=True, drop=True) 
+    final_df.to_csv('Steam Game Details.csv')
+
+
+if __name__ == "__main__":
+    main()
---
+++ flask-app.py
@@ -0,0 +1,63 @@
+from flask import Flask
+from flask import render_template
+import ast
+import pandas as pd
+import numpy as np
+import json
+from urllib.request import urlopen
+
+import pickle
+games = pickle.load(open('Games.pkl', 'rb'))
+games_data = games[['appid', 'title']].to_json(orient="records")
+
+similarity = pickle.load(open('Similarity.pkl', 'rb'))
+
+def recommendGame(game_index):
+   recommended_games = []
+   distances = sorted(list(enumerate(similarity[game_index])), reverse=True, key=lambda x: x[1])
+   
+   for i in distances[1:7]:
+      recommended_games.append({'appid': games.iloc[i[0]].appid, 'title': games.iloc[i[0]].title})
+
+   return recommended_games
+
+def getSelectedGameData(appid):
+   res = urlopen(f'https://store.steampowered.com/api/appdetails?appids={appid}&l=english')
+   res_json = json.load(res)
+   json_data = res_json[f'{appid}']['data']
+
+   return json_data
+
+title = 'Steam Game Recommender'
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('index.html', games_data = games_data, title=title)
+
+@app.route('/game/<appid>')
+def game(appid):
+   game_index = games[games['appid']==int(appid)].index[0]
+   recommended_games = recommendGame(game_index)
+
+   json_data = getSelectedGameData(appid)
+
+   selected_game_data = {
+      'title': json_data['name'],
+      'description': json_data['short_description'],
+      'is_free': json_data['is_free'],
+      'supported_languages': json_data['supported_languages'],
+      'developers': json_data['developers'],
+      'publishers': json_data['publishers'],
+      'platforms': json_data['platforms'],
+      'categories': json_data['categories'],
+      'genres': json_data['genres'],
+      'release_date': json_data['release_date']['date'],
+   }
+
+   return render_template('game.html', title = title, appid = appid, recommended_games = recommended_games, selected_game_data = selected_game_data)
+ 
+# main driver function
+if __name__ == '__main__':
+   app.run()
